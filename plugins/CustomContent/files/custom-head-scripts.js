abd2205f32e630d55129916be9b09f870bb4ebfd
Chris Carey <503084+oobi@users.noreply.github.com>
2026-03-26 11:44:20 +1100

    reduce opacity

--- plugins/CustomContent/files/custom-head-scripts.js
+++ plugins/CustomContent/files/custom-head-scripts.js
@@ -4,7 +4,7 @@ $(function () {
         var $this = $(this);
         var col = $this.css("color");
         var a = col.slice(4).split(",");
-        var newAlpha = 0.3;
+        var newAlpha = 0.2;
         var bgCol = "rgba(" + a[0] + "," + parseInt(a[1]) + "," + parseInt(a[2]) + "," + newAlpha + ")";
         $this.closest("tr").css("background-color", bgCol);
     });
